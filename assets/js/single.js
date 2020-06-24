946747c95522354d5b8c217fb45339543d52c921
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-06-24 17:34:24 -0500

    link the js pages, create error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 var issuesContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent=repoName;
+        getRepoIssues(repoName);
+        
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html")
+    }
+}
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -16,9 +33,9 @@ var getRepoIssues = function (repo) {
                     displayWarning(repo)
                 }
             })
-        }
-        else {
-            alert("There was a problem with your request!");
+        } else {
+            // if not successful, redirecto to homepage
+            document.location.replace("./index.html")
         }
     })
 }
@@ -74,4 +91,4 @@ var displayWarning = function (repo) {
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+getRepoName();
